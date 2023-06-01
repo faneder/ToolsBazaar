2266cdf35f125fdd3ba817430192ac9ff24fa2e0
Eder Fan <fanedercode@gmail.com>
2023-06-01 21:42:40 +0100

    Add most expensive endpoint that sorted by highest price then alphabetically name

--- ToolsBazaar.Persistence/ProductRepository.cs
+++ ToolsBazaar.Persistence/ProductRepository.cs
@@ -5,4 +5,12 @@ namespace ToolsBazaar.Persistence;
 public class ProductRepository : IProductRepository
 {
     public IEnumerable<Product> GetAll() => DataSet.AllProducts;
+    public IEnumerable<Product> FindAllByPriceDescThenByName()
+    {
+        {
+            return DataSet.AllProducts
+                .OrderByDescending(p => p.Price)
+                .ThenBy(p => p.Name);
+        }
+    }
 }
--- ToolsBazaar.Web/Controllers/ProductsController.cs
+++ ToolsBazaar.Web/Controllers/ProductsController.cs
@@ -1,7 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToolsBazaar.Domain.ProductAggregate;
 
 namespace ToolsBazaar.Web.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProductsController : ControllerBase { }
+public class ProductsController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger, IProductRepository productRepository)
+    {
+        _logger = logger;
+        _productRepository = productRepository;
+    }
+
+    [HttpGet("most-expensive")]
+    public IActionResult GetMostExpensiveProducts()
+    {
+        _logger.LogInformation("Retrieving the most expensive products...");
+
+        var products = _productRepository.FindAllByPriceDescThenByName();
+
+        return Ok(products);
+    }
+}
